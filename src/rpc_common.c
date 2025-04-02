991306539b4042f0818323fa94e26d7ef664e291
Jongyul Kim <yulistic@gmail.com>
2025-04-02 14:31:26 +0900

    Remove hidden sleep.

--- src/rpc_common.c
+++ src/rpc_common.c
@@ -26,8 +26,8 @@ uint64_t alloc_msgbuf_id(struct rpc_ch_info *rpc_ch)
 		if (ret)
 			break;
 		else {
-			log_info("Failed to alloc a msgbuf id. (sleep 1 sec)");
-			sleep(1);
+			log_warn("Failed to alloc a msgbuf id. (sleep 1 sec)");
+			// sleep(1);
 		}
 	}
 
